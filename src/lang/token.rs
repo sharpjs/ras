75480e438dbbab604a9c42f0e73abfc37e610d94
Jeff Sharp <jeff.sharp@git>
2019-05-03 06:56:36 -0700

    Begin tokens

--- src/lang/token.rs
+++ src/lang/token.rs
@@ -14,3 +14,62 @@
 // You should have received a copy of the GNU General Public License
 // along with ras.  If not, see <http://www.gnu.org/licenses/>.
 
+/// A lexical token.
+#[derive(PartialEq, Eq, Debug)]
+pub enum Token {
+    /// Identifier with index into string table.
+    Ident(usize),
+
+    /// Label with index into string table.
+    Label(usize),
+
+    /// Integer literal.
+    Int(u64),
+    // TODO: Non-two's-complement representations
+    // TODO: Float and its representations
+
+    /// Character literal.
+    Char(char),
+
+    /// String literal.
+    Str(usize),
+
+    /// Plus `+`.
+    Plus,
+
+    /// Minus `-`.
+    Minus,
+
+    // TODO: More operators
+
+    /// Left brace `{`.
+    BraceL,
+
+    /// Right brace `}`.
+    BraceR,
+
+    /// Left parenthesis `(`.
+    ParenL,
+
+    /// Right parenthesis `)`.
+    ParenR,
+
+    /// Left bracket  `[`.
+    BracketL,
+
+    /// Right bracket `]`.
+    BracketR,
+
+    /// Comma `,`.
+    Comma,
+
+    /// End of statement.
+    Eos,
+
+    /// End of file.
+    Eof,
+
+    /// Lexical error.
+    Error
+}
+

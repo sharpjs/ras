346aadf27452d30731761a6ed0306d6ae962267e
Jeff Sharp <jeff.sharp@git>
2019-05-03 06:56:10 -0700

    Add lang submodule.

--- src/lang/lexer.rs
+++ src/lang/lexer.rs
@@ -14,11 +14,3 @@
 // You should have received a copy of the GNU General Public License
 // along with ras.  If not, see <http://www.gnu.org/licenses/>.
 
-#![allow(dead_code)]
-
-mod arch;
-mod lang;
-
-fn main() {
-}
-

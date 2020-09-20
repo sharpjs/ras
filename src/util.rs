20477573e1a6b33fd66b1a1fb13173ce86d9dcea
Jeff Sharp <public@sharpjs.net>
2020-09-20 13:49:35 -0700

    Add Location.

--- src/util.rs
+++ src/util.rs
@@ -13,3 +13,74 @@
 //
 // You should have received a copy of the GNU General Public License
 // along with ras.  If not, see <http://www.gnu.org/licenses/>.
+
+use std::fmt::{self, Display, Formatter};
+
+/// A source code location.
+#[derive(Copy, Clone, PartialEq, Eq, PartialOrd, Ord, Debug)]
+pub struct Location {
+    /// The 1-based line number, or `0` to indicate an unknown line.
+    pub line: u32,
+
+    /// The 1-based column number, or `0` to indicate an unknown column.
+    pub column: u32,
+}
+
+impl Location {
+    pub const UNKNOWN: Self = Self::new(0, 0);
+    pub const BOF:     Self = Self::new(1, 1);
+
+    pub const fn new(line: u32, column: u32) -> Self {
+        Self { line, column }
+    }
+
+    /*
+    pub fn inc_line(&mut self) {
+        self.line += 1
+    }
+
+    pub fn inc_column(&mut self) {
+        self.column += 1
+    }
+    */
+}
+
+impl Display for Location {
+    fn fmt(&self, f: &mut Formatter) -> fmt::Result {
+        match (self.line, self.column) {
+            (0, 0) => Ok(()),
+            (l, 0) => write!(f, "{}",    l   ),
+            (l, c) => write!(f, "{}:{}", l, c),
+        }
+    }
+}
+
+#[cfg(test)]
+mod test {
+    use super::*;
+
+    #[test]
+    fn location_unknown() {
+        assert_eq!( Location::UNKNOWN, Location::new(0, 0) );
+    }
+
+    #[test]
+    fn location_bof() {
+        assert_eq!( Location::BOF, Location::new(1, 1) );
+    }
+
+    #[test]
+    fn location_display_fmt_0_0() {
+        assert_eq!( format!("{}", Location::new(0, 0)), "" );
+    }
+
+    #[test]
+    fn location_display_fmt_n_0() {
+        assert_eq!( format!("{}", Location::new(1, 0)), "1" );
+    }
+
+    #[test]
+    fn location_display_fmt_n_n() {
+        assert_eq!( format!("{}", Location::new(1, 2)), "1:2" );
+    }
+}

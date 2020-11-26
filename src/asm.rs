f28449526e07f5ba141066293f927c9d00403b40
Jeff Sharp <public@sharpjs.net>
2020-11-26 10:50:58 -0800

    Add top-level Assembler type.

---
+++ src/asm.rs
@@ -0,0 +1,115 @@
+//! Top-Level Assembler
+//
+// This file is part of ras, an assembler.
+// Copyright 2020 Jeffrey Sharp
+//
+// ras is free software: you can redistribute it and/or modify it
+// under the terms of the GNU General Public License as published
+// by the Free Software Foundation, either version 3 of the License,
+// or (at your option) any later version.
+//
+// ras is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See
+// the GNU General Public License for more details.
+//
+// You should have received a copy of the GNU General Public License
+// along with ras.  If not, see <http://www.gnu.org/licenses/>.
+
+//use std::fmt::{self, Display, Formatter};
+use std::fs;
+use std::io::{stdin, stdout, Read, Write};
+
+use crate::message::*;
+use crate::message::Severity::*;
+
+/// The type returned by fallible assembler methods.
+pub type Result<T=(), E=()> = std::result::Result<T, E>;
+
+/// Top-level assembler interface.
+#[derive(Debug)]
+pub struct Assembler {
+    output:        Vec<u8>,
+    warning_count: u16,
+    error_count:   u16,
+}
+
+impl Assembler {
+    /// Creates a new assembler.
+    pub fn new() -> Self {
+        Self {
+            output:        Vec::with_capacity(16 * 1024),
+            warning_count: 0,
+            error_count:   0,
+        }
+    }
+
+    pub fn result(&self) -> Result {
+        match self.error_count {
+            0 => Ok  (()),
+            _ => Err (()),
+        }
+    }
+
+    /// Prints an assembler message to the standard error stream.
+    pub fn print(&mut self, msg: &Message) {
+        match msg.severity {
+            Normal  => (),
+            Warning => self.warning_count += 1,
+            _       => self.error_count   += 1,
+        };
+        eprintln!("{}", msg)
+    }
+
+    /// Assembles the file at the given `path`.
+    pub fn assemble_file(&mut self, path: &str) -> Result {
+        match fs::read_to_string(path) {
+            Ok (s) => self.assemble_bytes(path, s.as_bytes()),
+            Err(e) => {
+                // TODO: Message type needs to be fixed
+                eprintln!("{}: error: {}", path, e);
+                Err(())
+            },
+        }
+    }
+
+    /// Assembles the bytes read from standard input.
+    pub fn assemble_stdin(&mut self) -> Result {
+        self.assemble_from("stdin", stdin())
+    }
+
+    /// Assembles the bytes read from `src`, using `path` as the pathname.
+    pub fn assemble_from<R: Read>(&mut self, path: &str, mut src: R) -> Result {
+        let mut s = String::new();
+        match src.read_to_string(&mut s) {
+            Ok (_) => self.assemble_bytes(path, s.as_bytes()),
+            Err(e) => {
+                // TODO: Message type needs to be fixed
+                eprintln!("{}: error: {}", path, e);
+                Err(())
+            },
+        }
+    }
+
+    /// Assembles the given `bytes`, using `path` as the pathname.
+    pub fn assemble_bytes(&mut self, path: &str, bytes: &[u8]) -> Result {
+        // TODO: Everything.  For now just copy input to output.
+        self.output.extend_from_slice(path.as_bytes());
+        self.output.push(b':');
+        self.output.push(b'\n');
+        self.output.extend_from_slice(bytes);
+
+        if self.error_count == 0 { Ok(()) } else { Err(()) }
+    }
+
+    pub fn write_output(&mut self) -> Result {
+        match stdout().write_all(&self.output) {
+            Ok (_) => Ok(()),
+            Err(e) => {
+                // TODO: Message type needs to be fixed
+                eprintln!("ras: error: {}", e);
+                Err(())
+            },
+        }
+    }
+}
--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,7 @@
+//! Program Entry Point and Crate Root
+//
 // This file is part of ras, an assembler.
-// Copyright (C) 2020 Jeffrey Sharp
+// Copyright 2020 Jeffrey Sharp
 //
 // ras is free software: you can redistribute it and/or modify it
 // under the terms of the GNU General Public License as published
@@ -17,6 +19,7 @@
 #![allow(dead_code)]
 
 mod arch;
+mod asm;
 mod lang;
 mod mem;
 mod message;
@@ -25,34 +28,48 @@ mod util;
 mod value;
 
 use std::env::args;
-use std::io::{self, stdin, stdout, stderr, Read, Write};
-use std::fs::File;
+use std::process::exit;
+
+use self::asm::{Assembler, Result};
 
 /// The name of the assembler.
 pub const PROGRAM_NAME: &str = "ras";
 
-fn main() -> io::Result<()> {
+fn main() {
+    if run().is_err() { exit(1) }
+}
+
+fn run() -> Result {
+    // This function translates command-line arguments into actions that
+    // configure, invoke, and save the output of the assembler.
     let mut args = args();
+
+    // Ignore the first argument (usually executable path, but not guaranteed).
     args.next();
 
-    let mut buffer = String::new();
+    // The Assembler type provides a facade for the assembler.  It provides a
+    // method for each command-line option.
+    let mut asm = Assembler::new();
 
+    // TODO: Process non-source-file arguments here.
+
+    // Consume source files.
     if args.len() == 0 {
-        writeln!(stderr(), "reading stdin")?;
-        stdin().read_to_string(&mut buffer)?;
+        asm.assemble_stdin()?;
     } else {
         for arg in args {
             if arg == "-" {
-                writeln!(stderr(), "reading stdin")?;
-                stdin().read_to_string(&mut buffer)?;
+                asm.assemble_stdin()?;
             } else {
-                writeln!(stderr(),"reading {}", arg)?;
-                File::open(arg)?.read_to_string(&mut buffer)?;
+                asm.assemble_file(&arg)?;
             }
         }
     }
 
-    write!(stdout(), "{}", buffer)?;
-    Ok(())
-}
+    // Write output
+    if asm.result().is_ok() {
+        asm.write_output()?;
+    }
 
+    asm.result()
+}
